c3a98fb2d791cf89e7d905c855ef343feb95a6d8
897895737@qq.com <897895737@qq.com>
2017-03-08 16:03:20 +0800

    task7

---
+++ JavaScript+/public.js
@@ -0,0 +1,38 @@
+var $ = function(el) {
+    return document.querySelector(el);
+}
+
+var $$ = function(el) {
+    return document.querySelectorAll(el);
+}
+
+var createEle = function(tagName) {
+    return document.createElement(tagName);
+}
+
+
+function addEvent(ele, event, hanlder) {
+    if (ele.addEventListener) {
+        ele.addEventListener(event, hanlder, false);
+    } 
+    else if (ele.attachEvent) {
+        ele.attachEvent('on' + event, hanlder);
+    } 
+    else 
+    {
+        ele['on' + event] = hanlder;
+    }
+}
+
+function removeEvent(ele, event, hanlder) {
+    if (ele.removeEventListener) {
+        ele.removeEventListener(event, hanlder, false);
+    } 
+    else if (ele.detachEvent) {
+        ele.detachEvent('on' + event, hanlder);
+    } 
+    else 
+    {
+        ele['on' + event] = null;
+    }
+}
